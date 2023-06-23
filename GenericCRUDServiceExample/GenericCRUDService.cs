a78d2643c32a49bcd1cf0733f8f44694aa28ebfc
A.Waris <andrew.waris@convergetp.com>
2023-06-23 12:29:30 +0500

    Refactor and register generic service

--- GenericCRUDServiceExample/GenericCRUDService.cs
+++ GenericCRUDServiceExample/GenericCRUDService.cs
@@ -1,19 +1,19 @@
 using System.Linq.Expressions;
+using System.Reflection;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 
 
 namespace GenericCRUDServiceExample;
 
+
 public interface IGenericCRUDService<TModel, TDto>
 {
-    Task<IEnumerable<TDto>> GetAll(Expression<Func<TModel, bool>>? where = null, params string[] includes);
-    Task<TDto> GetById(int id);
+    Task<IEnumerable<TDto>> GetAll(Expression<Func<TModel, bool>> where = null, params string[] includes);
+    Task<TDto> GetById(int id, params string[] includes);
     Task<TDto> Add(TDto dto, params Expression<Func<TModel, object>>[] references);
-
-    Task<TDto> Update(int id, TDto dto, Expression<Func<TModel, bool>>? where = null,
+    Task<TDto> Update(int id, TDto dto, Expression<Func<TModel, bool>> where = null,
         params Expression<Func<TModel, object>>[] references);
-
     Task<bool> Delete(int id);
 }
 
@@ -23,37 +23,33 @@ public class GenericCRUDService<TModel, TDto> : IGenericCRUDService<TModel, TDto
 {
     private readonly IMapper _mapper;
     private readonly DbContext _dbContext;
+    private readonly PropertyInfo _keyProperty;
 
     public GenericCRUDService(IMapper mapper, DbContext dbContext)
     {
         _mapper = mapper;
         _dbContext = dbContext;
+        _keyProperty = GetKeyProperty();
     }
 
-    // Example 1: var dtos = await _genericService.GetAll("NavigationProperty1", "NavigationProperty2");
-    // Example 2: var customers = await _customerService.GetAll(c => c.Age > 18 && c.IsActive, "Orders");
-
-    public async Task<IEnumerable<TDto>> GetAll(Expression<Func<TModel, bool>>? where = null,
+    public async Task<IEnumerable<TDto>> GetAll(Expression<Func<TModel, bool>> where = null,
         params string[] includes)
     {
-        var query = _dbContext.Set<TModel>().AsQueryable();
+        var query = ApplyIncludes(_dbContext.Set<TModel>(), includes);
 
-        // Apply the 'where' predicate if provided
         if (where != null)
         {
             query = query.Where(where);
         }
 
-        // Include related entities if the 'includes' parameter is provided
-        query = includes.Aggregate(query, (current, include) => current.Include(include));
-
         var entities = await query.ToListAsync();
         return _mapper.Map<IEnumerable<TDto>>(entities);
     }
 
-    public async Task<TDto> GetById(int id)
+    public async Task<TDto> GetById(int id, params string[] includes)
     {
-        var entity = await _dbContext.Set<TModel>().FindAsync(id);
+        var query = ApplyIncludes(_dbContext.Set<TModel>(), includes);
+        var entity = await query.FirstOrDefaultAsync(e => GetKeyValue(e) == id);
         return _mapper.Map<TDto>(entity);
     }
 
@@ -62,54 +58,20 @@ public class GenericCRUDService<TModel, TDto> : IGenericCRUDService<TModel, TDto
         var entity = _mapper.Map<TModel>(dto);
         _dbContext.Set<TModel>().Add(entity);
 
-        // Include related entities if the 'references' parameter is provided
-        foreach (var reference in references)
-        {
-            await _dbContext.Entry(entity).Reference(reference!).LoadAsync();
-        }
-
+        await LoadReferences(entity, references);
         await _dbContext.SaveChangesAsync();
+
         return _mapper.Map<TDto>(entity);
     }
 
-    // Example: var updatedDto = await _genericService.Update(1, dto, c => c.IsActive, c => c.User);
-    public async Task<TDto> Update(int id, TDto dto,
-        Expression<Func<TModel, bool>>? where = null,
+    public async Task<TDto> Update(int id, TDto dto, Expression<Func<TModel, bool>> where = null,
         params Expression<Func<TModel, object>>[] references)
     {
-        var query = _dbContext.Set<TModel>().AsQueryable();
-
-        // Apply the 'where' predicate if provided
-        if (where == null)
-        {
-            // If no 'where' predicate is provided, findAsync the entity by id where id is the one that has [Key] annotation in the model so find the property that has [Key] annotation
-            var keyProperty = typeof(TModel).GetProperties().FirstOrDefault(p =>
-                p.CustomAttributes.Any(a =>
-                    a.AttributeType == typeof(System.ComponentModel.DataAnnotations.KeyAttribute)));
-            if (keyProperty == null)
-            {
-                throw new Exception("No [Key] attribute found in the model");
-            }
-
-            query = query.Where(
-                entity => (int)entity.GetType().GetProperty(keyProperty.Name)!.GetValue(entity)! == id);
-        }
-        else
-        {
-            // If a 'where' predicate is provided, apply it
-            query = query.Where(where);
-        }
-
-
+        var query = ApplyWhere(_dbContext.Set<TModel>(), where, id);
         var entity = await query.FirstOrDefaultAsync();
 
         _mapper.Map(dto, entity);
-
-        foreach (var reference in references)
-        {
-            if (entity != null) await _dbContext.Entry(entity).Reference(reference!).LoadAsync();
-        }
-
+        await LoadReferences(entity, references);
         await _dbContext.SaveChangesAsync();
 
         return _mapper.Map<TDto>(entity);
@@ -125,6 +87,46 @@ public class GenericCRUDService<TModel, TDto> : IGenericCRUDService<TModel, TDto
 
         _dbContext.Set<TModel>().Remove(entity);
         await _dbContext.SaveChangesAsync();
+
         return true;
+    }
+
+    private IQueryable<TModel> ApplyIncludes(IQueryable<TModel> query, IEnumerable<string> includes)
+    {
+        return includes.Aggregate(query, (current, include) => current.Include(include));
+    }
+
+    private IQueryable<TModel> ApplyWhere(IQueryable<TModel> query, Expression<Func<TModel, bool>> where, int id)
+    {
+        query = query.Where(where ?? (entity => GetKeyValue(entity) == id));
+
+        return query;
+    }
+
+    private int GetKeyValue(TModel entity)
+    {
+        return (int)_keyProperty.GetValue(entity)!;
+    }
+
+    private async Task LoadReferences(TModel entity, IEnumerable<Expression<Func<TModel, object>>> references)
+    {
+        foreach (var reference in references)
+        {
+            await _dbContext.Entry(entity).Reference(reference).LoadAsync();
+        }
+    }
+
+    private static PropertyInfo GetKeyProperty()
+    {
+        var keyProperty = typeof(TModel).GetProperties().FirstOrDefault(p =>
+            p.CustomAttributes.Any(a =>
+                a.AttributeType == typeof(System.ComponentModel.DataAnnotations.KeyAttribute)));
+
+        if (keyProperty == null)
+        {
+            throw new Exception("No [Key] attribute found in the model");
+        }
+
+        return keyProperty;
     }
 }
--- GenericCRUDServiceExample/Program.cs
+++ GenericCRUDServiceExample/Program.cs
@@ -1,4 +1,8 @@
+using GenericCRUDServiceExample;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped(typeof(IGenericCRUDService<,>), typeof(GenericCRUDService<,>));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
